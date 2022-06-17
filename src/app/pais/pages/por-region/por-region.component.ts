a9606429be09d3cc0c039e9ad07052b67097675f
nery1010 <84634161+nery1010@users.noreply.github.com>
2022-06-17 18:25:44 -0500

    peticiones http

--- src/app/pais/pages/por-region/por-region.component.ts
+++ src/app/pais/pages/por-region/por-region.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Country } from '../../interfaces/pai.interface';
+import { PaisService } from '../../services/pais.service';
 @Component({
   selector: 'app-por-region',
   templateUrl: './por-region.component.html',
@@ -8,13 +9,36 @@ import { Component, OnInit } from '@angular/core';
 export class PorRegionComponent implements OnInit {
 
   
-  capital = [];
-  termino: string = '';
-  hayError: boolean = false;
+  
 
-  constructor() { }
+  regiones: string[] = ['africa', 'americas', 'asia', 'europe', 'oceania'];
+  regionActiva: string="";
+  paises: Country[] = [];
+
+  
+  constructor(private paisService: PaisService) { }
 
   ngOnInit(): void {
   }
+
+
+  getClassCss(region: string){
+    return (region===this.regionActiva) ? 'btn btn-primary' : 'btn btn-outline-primary';
+
+  }
+
+  activarRegion(region: string){
+    if(region === this.regionActiva) {return;}
+    this.regionActiva = region;
+    this.paises = [];
+    console.log(this.regionActiva);
+    this.paisService.buscarRegion(region).subscribe(paises =>{
+      console.log(paises);
+       this.paises = paises
+      });
+    
+  }
+
+
 
 }
